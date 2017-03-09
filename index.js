0acc4a5e4b9914946a0df03ecc305c16bd23b13d
Huafu Gandon <huafu.gandon@gmail.com>
2017-03-09 14:24:36 +0700

    upgrade feathers client version and location of socket.io-client script

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = {
     let feathersPath = require('path').join('lib', 'client.js');
     let ioPath = require('path').join('lib', 'index.js');
     feathersPath = require.resolve('feathers-client').replace(feathersPath, '') + 'dist/feathers.js';
-    ioPath = require.resolve('socket.io-client').replace(ioPath, '') + 'dist/socket.io.js';
+    ioPath = require.resolve('socket.io-client').replace(ioPath, '') + 'socket.io.js';
     app.import(feathersPath);
     app.import(ioPath);
     app.import('vendor/feathers.js', {
