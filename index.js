c68bbe0ab96cc20125b66d9389c127073761357b
Huafu Gandon <huafu.gandon@gmail.com>
2017-04-11 08:49:01 +0700

    fixes issue when addon inside addon

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ module.exports = {
   name: 'ember-data-feathers',
 
   included(app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    this.app = app;
+
     let feathersPath = require('path').join('lib', 'client.js');
     let ioPath = require('path').join('lib', 'index.js');
     feathersPath = require.resolve('feathers-client').replace(feathersPath, '') + 'dist/feathers.js';
